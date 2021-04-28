91cd3a2a0d971477144ad0a51405178ee41948c3
martinmunillas <martinmunillas@gmail.com>
2021-04-28 16:13:45 -0400

    WTW-736 feature: added more customization features

--- src/types.ts
+++ src/types.ts
@@ -13,4 +13,11 @@ export interface StyleConfig {
   between: string;
   disabled: string;
   normal: string;
+  background: string;
+  font: string;
+
+  /**
+   * this will add to the datepicker any css you want
+   */
+  custom?: string;
 }
