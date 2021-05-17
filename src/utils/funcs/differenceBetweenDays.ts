052de4df513ba36cc19e308cbabb9f0cfbce4a82
Martin Munilla <65319051+martinmunillas@users.noreply.github.com>
2021-05-17 11:35:06 -0400

    feature: added function to get the difference between two days (#5)
    * feature: added function to get the difference between two days
    
    * 0.1.1

---
+++ src/utils/funcs/differenceBetweenDays.ts
@@ -0,0 +1,12 @@
+import { Day } from "../../types";
+import { generateDate } from "./generateDate";
+
+/**
+ * Returns the difference between two datepicker Days
+ */
+export const differenceBetweenDays = (day1: Day, day2: Day) => {
+  const diffTime = Math.abs(
+    generateDate(day1).valueOf() - generateDate(day2).valueOf()
+  );
+  return Math.floor(diffTime / (1000 * 60 * 60 * 24));
+};
