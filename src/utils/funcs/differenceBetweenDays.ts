615f97e3988bc1137808949a368020267914470e
martinmunillas <martinmunillas@gmail.com>
2021-05-17 10:30:52 -0400

    feature: fixed function

--- src/utils/funcs/differenceBetweenDays.ts
+++ src/utils/funcs/differenceBetweenDays.ts
@@ -6,7 +6,7 @@ import { generateDate } from "./generateDate";
  */
 export const differenceBetweenDays = (day1: Day, day2: Day) => {
   const diffTime = Math.abs(
-    generateDate(day1).getSeconds() - generateDate(day2).getSeconds()
+    generateDate(day1).valueOf() - generateDate(day2).valueOf()
   );
   return Math.floor(diffTime / (1000 * 60 * 60 * 24));
 };
