dd07fd0033c5822a367ddf0f94414fe0d0d7da38
Miguel Cádiz <miguel@gowheeltheworld.com>
2022-05-30 18:03:55 -0400

    Change disabled styles and reset dates when a third one is selected

--- src/stories/DatePicker.stories.tsx
+++ src/stories/DatePicker.stories.tsx
@@ -1,42 +1,41 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Meta, Story } from '@storybook/react';
 import Datepicker, { DatePickerProps } from '../DatePicker';
-import { useToggle } from '../utils/hooks/useToggle';
-import { SelectedDates } from '../types/SelectedDates';
 import { generateDay } from '../utils/funcs/generateDay';
 
 export default {
     title: 'Atoms/DatePicker',
     component: Datepicker,
+    argTypes: {
+        isOpen: {
+            control: {
+                type: 'boolean'
+            },
+            defaultValue: true
+        },
+        multipleSelect: {
+            control: {
+                type: 'boolean'
+            }
+        },
+        value: {
+            control: { type: 'object' },
+            defaultValue: []
+        },
+        minDate: {
+            control: { type: 'object' }
+        },
+        maxDate: {
+            control: { type: 'object' }
+        },
+    }
 } as Meta;
 
 const Template: Story<DatePickerProps> = (args) => {
-    const [open, toggle] = useToggle(true);
-    const [multiple, toggleMultiple] = useToggle(true);
-    const [date, setDate] = useState<SelectedDates>([null, null]);
     return (
-        <>
-            <button onClick={() => toggle()}>
-                {date[0]
-                    ? `Selected from ${date[0].day}/${date[0].month}/${date[0].year}
-          ${
-              date[1] &&
-              `to ${date[1].day}/${date[1].month}/
-          ${date[1].year}`
-          }`
-                    : 'Datepicker'}
-            </button>
-            <button onClick={() => toggleMultiple()}>toggle multiple</button>
-
-            <Datepicker
+        <Datepicker
                 {...args}
-                isOpen={open}
-                handleToggle={toggle}
-                onChange={setDate}
-                value={date}
-                multipleSelect={multiple}
             />
-        </>
     );
 };
 
