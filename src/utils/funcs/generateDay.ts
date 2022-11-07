dd76a009630f0d3266a6a47005fb51f3369dc37a
ferpolwheel <113117534+ferpolwheel@users.noreply.github.com>
2022-11-07 11:38:58 -0300

    Pltfrm-40/optimize library bundle (#34)
    * revert last commit changes
    
    * increase version

--- src/utils/funcs/generateDay.ts
+++ src/utils/funcs/generateDay.ts
@@ -4,7 +4,7 @@ import { Day } from '../../types/Day';
  * Gets a javascript Date and returns a datepicker Day
  */
 export const generateDay = (date: Date): Day => ({
-    day: date.getUTCDate(),
-    month: date.getUTCMonth() + 1,
-    year: date.getUTCFullYear(),
+    day: date.getDate(),
+    month: date.getMonth() + 1,
+    year: date.getFullYear(),
 });
