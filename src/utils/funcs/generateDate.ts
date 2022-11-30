bafbe5d3ba7682071e4319629fc3e306bcd6a36b
Harold <harold@gowheeltheworld.com>
2022-11-30 13:09:24 -0500

    add comments about UTC

--- src/utils/funcs/generateDate.ts
+++ src/utils/funcs/generateDate.ts
@@ -1,7 +1,11 @@
 import { Day } from '../../types/Day';
 
 /**
- *  Transform a DatePicker's `Day` into a JS's `Date`
+ * Transform a DatePicker's `Day` into a JS's `Date`.
+ *
+ * UTC time was used to ensure resulting JS's `Date` represent the beginning of the
+ * day. Please use UTC-equivalent JSDate's methods on the resulting `Date`.
+ * Otherwise timezone related bugs may appear.
  */
 export const generateDate = ({ year, month, day }: Day): Date =>
     // Month in `Day` type are 1-based indexed
