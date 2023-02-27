cc501867c2061e649df02370419f7a8085743f9f
ferpolwheel <113117534+ferpolwheel@users.noreply.github.com>
2023-02-27 10:54:55 -0300

    Fix/pts 887 revert 0.10 version 2 (#39)
    * revert 0.5.10
    
    * UTC gunction added, version modified
    
    * export generateUTCDay in index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { default as Datepicker, DatePickerProps } from './DatePicker';
 export { useToggle } from './utils/hooks/useToggle';
-export { generateDay } from './utils/funcs/generateDay';
+export { generateDay, generateUTCDay } from './utils/funcs/generateDay';
 export { generateDate } from './utils/funcs/generateDate';
 export { differenceBetweenDays } from './utils/funcs/differenceBetweenDays';
 export type { SelectedDates } from './types/SelectedDates';
